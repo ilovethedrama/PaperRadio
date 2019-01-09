ce6dff0a7ad74ddb74fa7d5fc70ca316d3c267b0
Status100 <bluemagicuk@yahoo.co.uk>
2019-01-09 02:22:47 +0000

    adding JS file and selecting show button in the DOM

---
+++ JS/radio.js
@@ -0,0 +1,8 @@
+var showBtn = document.getElementById('Shows');
+
+var extraBit = document.getElementById('extra');
+
+showBtn.addEventListener('click', function(){
+    alert('you clicked the show button, so...it works innit');
+});
+
