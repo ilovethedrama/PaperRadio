5e1c3e16fe301c555e79a7feaf148cc9842c1c85
Status100 <bluemagicuk@yahoo.co.uk>
2019-01-12 15:46:11 +0000

    basic JS to allow the menu buttons to open and close pop out menu

--- JS/radio.js
+++ JS/radio.js
@@ -1,9 +1,47 @@
-var showBtn = document.getElementById('Shows');
+var showBtn = document.getElementById('showsBtn');
+var scheduleBtn = document.getElementById('scheduleBtn');
+var playlistBtn = document.getElementById('playlistBtn');
+var newsBtn = document.getElementById('newsBtn');
+var enterpriseBtn = document.getElementById('enterpriseBtn');
+var contactBtn = document.getElementById('contactBtn');
+var aboutBtn = document.getElementById('aboutBtn');
+var joinBtn = document.getElementById('joinBtn');
+var newsletterBtn = document.getElementById('newsletterBtn');
 
 var x = document.getElementById('extra');
 
 showBtn.addEventListener('click', function(){
     x.classList.toggle("extra2");
 });
+
+scheduleBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+playlistBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+newsBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+enterpriseBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+contactBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+aboutBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+joinBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+newsletterBtn.addEventListener('click', function(){
+    x.classList.toggle("extra2");
+});
+
+
+function openSideMenu(){
+//add this function to all buttons on nav to open menu and load info of their profile.
+}
 
 
